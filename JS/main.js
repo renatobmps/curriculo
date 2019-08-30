fd3140f41bb023753ac514098c612d3e0b85496b
renatobmpsilva <renatobmpsilva@gmail.com>
2019-08-30 00:13:59 -0300

    mensagem de boas vindas

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,1 @@
-var titulos = document.querySelectorAll("h2");
-console.log(titulos);
-
-for(var i = 0 ; i <  titulos.length ; i++){
-    var titulo = titulos[i];
-    titulo.classList.add("titulo");
-}
+alert("Seja muito bem vindo! :D")
