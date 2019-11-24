19c4b122817ad35a65db84866b20c35596aa4a6b
Renato <renatobmpsilva@gmail.com>
2019-11-24 01:05:21 -0300

    CSS refeito

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
+var data = new Date();
+var dia = parseInt(data.getDate());
+var mes = parseInt(data.getMonth()+1);
+var ano = parseInt(data.getFullYear());
+console.log("Data atual: "+dia+"/"+mes+"/"+ano);
+var diaNasc = parseInt(16);
+var mesNasc = parseInt(11);
+var anoNasc = parseInt(1993);
+console.log("Nascimento: "+diaNasc+"/"+mesNasc+"/"+anoNasc);
+
+var idade = parseInt(0);
+
+if (mes >= mesNasc && dia >= diaNasc) {
+    idade = ano - anoNasc;
+}else{
+    idade = (ano - anoNasc) - 1;
+}
+
+
 $(function inicializaSite() {
     console.log("jQuery funcionando ;)");
     $(".tooltip").tooltipster({
         theme: 'tooltipster-shadow'
     });
 })
+console.log("Idade: "+idade);
+
+$("#idade").text(idade)
