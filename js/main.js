dcfa18cfcf1113743c2f8d3cfa17e6367507cef3
Renato <renatobmpsilva@gmail.com>
2020-01-04 23:17:13 -0300

    icones de redes sociais e curso de responsivo

--- js/main.js
+++ js/main.js
@@ -2,17 +2,31 @@ var data = new Date();
 var dia = parseInt(data.getDate());
 var mes = parseInt(data.getMonth()+1);
 var ano = parseInt(data.getFullYear());
-console.log("Data atual: "+dia+"/"+mes+"/"+ano);
+var hoje = dia+"/"+mes+"/"+ano;
+console.log("Data atual: "+hoje);
+
 var diaNasc = parseInt(16);
 var mesNasc = parseInt(11);
 var anoNasc = parseInt(1993);
-console.log("Nascimento: "+diaNasc+"/"+mesNasc+"/"+anoNasc);
+var nascimento = diaNasc+"/"+mesNasc+"/"+anoNasc;
+console.log("Nascimento: "+nascimento);
 
 var idade = parseInt(0);
 
-if (mes >= mesNasc && dia >= diaNasc) {
-    idade = ano - anoNasc;
-}else{
+if (mes >= mesNasc) {
+    if (mes = mesNasc && dia <= diaNasc) {
+        semAniversario();
+    } else {
+        comAniversario();
+    }
+} else {
+    semAniversario();
+}
+
+function comAniversario() {
+    idade = idade = ano - anoNasc;
+}
+function semAniversario() {
     idade = (ano - anoNasc) - 1;
 }
 
